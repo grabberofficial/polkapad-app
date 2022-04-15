9b71bf8032915bf2790bda79af2fdac181e7acb3
Fedor Kirpichev <retardedjs@evrone.com>
2022-04-15 12:58:35 +0300

    fix: remove deprecated styled comp

--- src/components/HeadingWithUnderline/HeadingWithUnderline.style.ts
+++
@@ -1,9 +0,0 @@
-import { Heading, styled } from '@chakra-ui/react';
-
-export const HeadingWithUnderlineStyled = styled(Heading)`
-  &::after {
-    content: '';
-    width: 43px;
-    height: 5px;
-  }
-`;
