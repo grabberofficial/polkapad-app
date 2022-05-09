b4ebc026612edf08cd85a83b530013ba396d4b53
ilyaviache <ilya.viache@gmail.com>
2022-05-09 18:58:10 +0700

    integrate usedapp wrapper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,21 @@
 import { memo } from 'react';
+import { ChainId, DAppProvider, Ropsten } from '@usedapp/core';
 import { AppProps } from 'next/app';
 import Providers from '@/shared/providers';
 import { MainLayout } from '@/layouts';
 import { Header } from '@/components';
 import { SWRConfig } from 'swr';
 import fetchJson from '@/lib/fetchJson';
+
+//TODO: fix process.env.NEXT_PUBLIC_ALCHEMY_API_KEY
+
+const config = {
+  networks: [Ropsten],
+  readOnlyChainId: ChainId.Ropsten,
+  readOnlyUrls: {
+    [ChainId.Ropsten]: `https://eth-ropsten.alchemyapi.io/v2/${'ylE0wN4_2ALWd8ZK7hgvDMi4eRCfskQY'}`,
+  },
+};
 
 const App = (props: AppProps): JSX.Element => {
   const { Component, pageProps } = props;
@@ -33,7 +44,9 @@ const AppWrapper = (props: any): JSX.Element => {
 
   return (
     <Providers>
-      <MemoApp {...rest} />
+      <DAppProvider config={config}>
+        <MemoApp {...rest} />
+      </DAppProvider>
     </Providers>
   );
 };
