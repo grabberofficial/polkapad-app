4037c0aaefe491e39c3ab3ad413fe0bc9cbb83e4
Fedor Kirpichev <retardedjs@evrone.com>
2022-06-09 19:49:53 +0300

    fix: notion tasks and stablity fixes

--- src/lib/hooks/useUser.ts
+++ src/lib/hooks/useUser.ts
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import Router from 'next/router';
+import { useRouter } from 'next/router';
 import useSWR from 'swr';
 import { User } from '@/pages/api/user';
 
@@ -8,21 +8,24 @@ export default function useUser({
   redirectIfFound = false,
 } = {}) {
   const { data: user, mutate: mutateUser } = useSWR<User>('/api/user');
+  const router = useRouter();
 
   useEffect(() => {
     // if no redirect needed, just return (example: already on /dashboard)
     // if user data not yet there (fetch in progress, logged in or not) then don't do anything yet
-    if (!redirectTo || !user) return;
+    if (!redirectTo.length) return;
 
     if (
       // If redirectTo is set, redirect if the user was not found.
-      (redirectTo && !redirectIfFound && !user?.isLoggedIn) ||
+      (redirectTo.length > 0 &&
+        !redirectIfFound &&
+        (!user?.isLoggedIn || user === undefined)) ||
       // If redirectIfFound is also set, redirect if the user was found
       (redirectIfFound && user?.isLoggedIn)
     ) {
-      Router.push(redirectTo);
+      router.push(redirectTo);
     }
-  }, [user, redirectIfFound, redirectTo]);
+  }, [user, redirectIfFound, redirectTo, router]);
 
   return { user, mutateUser };
 }
