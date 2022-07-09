15740e5f69488ddac3cee8ec7a35300ae9d5c38f
Arseniy Nikitochkin <arseniynikitochkin@ya.ru>
2022-07-09 13:43:10 +0300

    Feat/disable sentry (#90)
    * feat: disable sentry
    
    * feat: disable sentry

--- next.config.js
+++ next.config.js
@@ -1,26 +1,26 @@
 const withImages = require('next-images');
-const { withSentryConfig } = require('@sentry/nextjs');
+// const { withSentryConfig } = require('@sentry/nextjs');
 
-const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
-  authToken: process.env.SENTRY_AUTH_TOKEN,
-  silent: true, // Suppresses all logs
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-};
+// const sentryWebpackPluginOptions = {
+//   // Additional config options for the Sentry Webpack plugin. Keep in mind that
+//   // the following options are set automatically, and overriding them is not
+//   // recommended:
+//   //   release, url, org, project, authToken, configFile, stripPrefix,
+//   //   urlPrefix, include, ignore
+//   authToken: process.env.SENTRY_AUTH_TOKEN,
+//   silent: true, // Suppresses all logs
+//   // For all available options, see:
+//   // https://github.com/getsentry/sentry-webpack-plugin#options.
+// };
 
-module.exports = withSentryConfig(
-  withImages({
-    publicRuntimeConfig: {
-      serviceUrl: process.env.SERVICE_URL,
-      mailchimpId: process.env.MAILCHIMP_ID,
-      googleAnalyticsId: process.env.GOOGLE_ANALYTICS_ID,
-      environment: process.env.ENVIRONMENT,
-    },
-  }),
-  sentryWebpackPluginOptions,
-);
+// module.exports = withSentryConfig(
+module.exports = withImages({
+  publicRuntimeConfig: {
+    serviceUrl: process.env.SERVICE_URL,
+    mailchimpId: process.env.MAILCHIMP_ID,
+    googleAnalyticsId: process.env.GOOGLE_ANALYTICS_ID,
+    environment: process.env.ENVIRONMENT,
+  },
+});
+// sentryWebpackPluginOptions,
+// );
