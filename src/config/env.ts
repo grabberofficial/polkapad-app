cf07dc1d54e3d5db1640c96318f3b70b93c76898
i-kitaev <vnkitayev@gmail.com>
2022-08-25 14:52:30 +0200

    feat: Gleam widget (#127)

--- src/config/env.ts
+++ src/config/env.ts
@@ -6,5 +6,5 @@ export const serviceUrl = publicRuntimeConfig?.serviceUrl;
 export const googleAnalyticsId = publicRuntimeConfig?.googleAnalyticsId;
 export const mailchimpId = publicRuntimeConfig?.mailchimpId;
 export const facebookPixel = publicRuntimeConfig?.facebookPixel;
-export const facebookVerifyId = publicRuntimeConfig?.facebookVerifyId;
+export const gleamRewardUrl = publicRuntimeConfig?.gleamRewardUrl;
 export const environment = publicRuntimeConfig?.environment;
