9706323cd5ef951b610fd7f11cb04319b4b7a2d1
Arseniy Nikitochkin <arseniynikitochkin@ya.ru>
2022-09-02 07:01:58 +0300

    Fix/wrong network (#139)
    * fix: fix wrong network
    
    * fix: fix

--- src/utils/wallets.ts
+++ src/utils/wallets.ts
@@ -60,7 +60,7 @@ export const formatEtherBalance = (balance?: BigNumber | string) =>
   balance ? parseFloat(formatEther(balance)).toFixed(2) : '';
 
 export const checkIsPolkaWalletInstalled = (wallet: WalletMeta) => {
-  return !!window?.injectedWeb3?.[wallet.extensionName];
+  return window?.injectedWeb3?.[wallet.extensionName];
 };
 
 export const checkIsPolkadotInstalled = () => {
