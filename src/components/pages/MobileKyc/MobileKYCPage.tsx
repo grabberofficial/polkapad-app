a94bc5bd707d84305e00adfa32dc02ab85f52360
Arseniy Nikitochkin <arseniynikitochkin@ya.ru>
2022-10-06 19:10:19 +0300

    fix: fix build

--- src/components/pages/MobileKyc/MobileKYCPage.tsx
+++ src/components/pages/MobileKyc/MobileKYCPage.tsx
@@ -1,18 +1,8 @@
 import { Heading } from '@/components/common/HeadingWithUnderline/HeadingWithUnderline';
-import React, { useEffect } from 'react';
 import styled from '@emotion/styled';
 import { Text } from '@chakra-ui/react';
-import { MagicLinkTypes } from '@/pages/api/magic-links';
-import { API_MAGIC_LINKS_ROUTE } from '@/constants/routes';
 
 export const MobileKycPage = () => {
-  useEffect(() => {
-    fetch(API_MAGIC_LINKS_ROUTE, {
-      method: 'POST',
-      body: JSON.stringify({ type: MagicLinkTypes.KYC }),
-    });
-  }, []);
-
   return (
     <>
       <Heading
