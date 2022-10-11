03ec06b6d420e40a175968627b8b7433734fcc80
Arseniy Nikitochkin <arseniynikitochkin@ya.ru>
2022-10-11 04:52:40 +0300

    feat: improve onboarding (#165)
    * feat: improve onboarding
    
    * feat: improve onboarding

--- src/components/pages/Login/LoginPage.schema.ts
+++ src/components/pages/Login/LoginPage.schema.ts
@@ -1,8 +1,14 @@
 import { object, string } from 'yup';
 
-export const LoginPageSchema = object()
+export const PasswordLoginPageSchema = object()
   .shape({
     email: string().required('Email is required').email('Email is invalid'),
     password: string().required('Password is required'),
+  })
+  .required();
+
+export const CodeLoginPageSchema = object()
+  .shape({
+    email: string().required('Email is required').email('Email is invalid'),
   })
   .required();
